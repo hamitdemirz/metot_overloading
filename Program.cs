deca04f58ca4f829422f2356f39da71225eade82
Hamit Demir <82902117+hamitdemirz@users.noreply.github.com>
2023-04-06 01:23:25 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+
+namespace metot_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //out parametreler
+            string sayi = "999";
+
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if(sonuc)
+            {
+                Console.WriteLine("Basarili");
+                Console.WriteLine(outSayi);
+            }
+            else {
+                Console.WriteLine("basarisiz");
+            }
+
+            TMetotlar instance = new.Metotlar();
+            instance.Topla(2,3, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+
+            //metot overloading
+
+            int ifade = 999;
+            instance.EkranaYazdir(Convert.ToString(ifade));
+            instance.EkranaYazdir(ifade);
+
+            //metot imzasi
+            
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a+b;
+        }
+
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+    }
+}
